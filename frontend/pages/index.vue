ce3506d37503b620b08754392c1456e32aebeaed
Kevin Do <do.kevin.vo@gmail.com>
2023-09-11 01:42:58 -0700

    :lipstick: show list of restaurants on ui

---
+++ frontend/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <article>
+    <h1>Restaurants</h1>
+    <ul>
+      <li v-for="restaurant in restaurants">
+        <h3>
+          {{ restaurant.attributes.Name }}
+        </h3>
+        <p>
+          {{ restaurant.attributes.Description }}
+        </p>
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script setup lang="ts">
+const result = await useFetch("http://localhost:1337/api/restaurants");
+
+const restaurants = result.data.value.data;
+</script>
