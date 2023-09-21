bdff0d86f0be4258d81bbb46febe7a30f9fed34d
Kevin Do <do.kevin.vo@gmail.com>
2023-09-21 01:45:26 -0700

    :rewind: ignore dist and .output

--- frontend/.output/public/_nuxt/default.6398b189.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as o,N as r,o as c}from"./entry.80d8908f.js";const n={};function s(e,a){return c(),o("div",null,[r(e.$slots,"default")])}const _=t(n,[["render",s]]);export{_ as default};
